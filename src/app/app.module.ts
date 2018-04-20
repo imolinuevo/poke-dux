e4ff75ad330e2d0df75da73d6f543568e4d20387
imolinuevo <imolinuevo@deloitte.es>
2018-04-20 12:21:19 +0200

    Add layout module, layut component, pokedex module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { LayoutModule } from './layout/layout.module';
+import { PokedexModule } from './pokedex/pokedex.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule,
+    PokedexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [LayoutComponent],
+  declarations: [LayoutComponent]
+})
+export class LayoutModule { }
