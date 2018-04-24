dba1680ff599a6b1d094a028d9a3c030ff03e4bf
imolinuevo <imolinuevo@deloitte.es>
2018-04-24 17:13:36 +0200

    Add pokemon list to pokedex and nest routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { LayoutModule } from './layout/layout.module';
+import { LayoutComponent } from './layout/layout/layout.component';
 import { PokedexModule } from './pokedex/pokedex.module';
+import { PokemonListComponent } from './pokedex/pokemon-list/pokemon-list.component';
+
+const appRoutes: Routes = [
+  { 
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'pokedex',
+        component: PokemonListComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +27,7 @@ import { PokedexModule } from './pokedex/pokedex.module';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     MaterialModule,
     LayoutModule,
     PokedexModule
--- src/app/pokedex/pokedex.module.ts
+++ src/app/pokedex/pokedex.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PokemonListComponent]
 })
 export class PokedexModule { }
