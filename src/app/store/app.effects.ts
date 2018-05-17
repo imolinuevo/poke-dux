65277d9be07233833717f5578fdd65c218f0a88e
imolinuevo <imolinuevo@deloitte.es>
2018-05-17 15:55:31 +0200

    Refactor effects

--- src/app/store/app.effects.ts
+++ src/app/store/app.effects.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
-import { mapTo, switchMap } from 'rxjs/operators';
-import * as pokedexPokemonActions from '../features/pokedex/store/pokedex-pokemon.actions';
-import * as bagPokemonActions from '../features/bag/store/bag-pokemon.actions';
+
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import * as bagPokemonActions from '../features/bag/store/bag-pokemon.actions';
+import * as pokedexPokemonActions from '../features/pokedex/store/pokedex-pokemon.actions';
 
 @Injectable()
 export class AppEffects {
